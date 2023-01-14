655b7a425095db0be0c6095dff87180232243583
Sergey Armyaninov <asm-techno2@yandex.ru>
2023-01-14 23:24:32 +0500

    Added All

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {21, 12, 43, 810 ,65, 688, 87, 38, 839, 810};
+
+int n = array.Length;
+
+int find = 810;
+
+int index = 0;
+while (index<n)
+{
+    if(array[index] == find)
+    {Console.WriteLine(index);
+    break;
+    }
+    index++;
+}
